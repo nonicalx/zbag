616a09b6d1e7215a288d4ddb4a16b240a1b2bd3d
nonicalx <nonicalx@gmail.com>
2019-11-19 13:43:46 +0100

    update

--- src/pages/userDashboard/userDashboard.js
+++ src/pages/userDashboard/userDashboard.js
@@ -4,7 +4,10 @@ import React from 'react'
 const UserDashboard = () => {
     return (
         <div>
-
+            <div className="row">
+                <div className='col-md-4 col-sm-12'></div>
+                <div className='col-md-8 col-sm-12'></div>
+            </div>
         </div>
     )
 }
